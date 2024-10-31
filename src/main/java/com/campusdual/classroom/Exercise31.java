a2725ece77fa93b6df1e42d4b2bc3e9c0d3db2a3
Tomás Mañas Fernández <tomas.manas.fernandez@gmail.com>
2024-10-31 15:52:59 +0100

    Ejercicio 31 finalizado

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,20 @@
 package com.campusdual.classroom;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        Path filePath = Paths.get("src/main/resources/lorem.txt");
 
+        try {
+            Files.lines(filePath).forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
     }
 }
+
